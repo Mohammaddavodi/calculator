9ff85100298f2e26d7d652ac618a3906db7e7b1f
Mohammaddavodi <Mohammaddavoodabadifarahani@gmail.com>
2024-03-02 19:58:23 +0330

    var Added

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var firstNumber:EditText
+    lateinit var secondNumber:EditText
+    lateinit var btnSum:EditText
+    lateinit var resultTv:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
